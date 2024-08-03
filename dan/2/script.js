b46c72a32415303e788f25a1d344112bbfa9b790
khazifire <73513866+khazifire@users.noreply.github.com>
2024-08-04 00:04:30 +0700

    refactor: format time with two digits

--- dan/2/script.js
+++ dan/2/script.js
@@ -8,8 +8,8 @@ setInterval(() => {
     const mins = date.getMinutes();
     const secs = date.getSeconds();
 
-    currentTime.innerHTML = `${hrs}:${mins}`;
-    seconds.innerHTML = secs;
+    currentTime.innerHTML = `${hrs>9?hrs: "0"+hrs}:${mins>9?mins: "0"+mins}`;
+    seconds.innerHTML = secs>9?secs: "0"+secs;
 
     const percentage = ((secs / 60) * 100) -4;
     sand.style.transform = `translateY(${percentage/2}%)`;
